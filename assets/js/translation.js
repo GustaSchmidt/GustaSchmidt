b8e2884b70497ec7efa842688a956e7b82c8059b
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2022-03-01 17:56:58 -0300

    Create translation.js

---
+++ assets/js/translation.js
@@ -0,0 +1,68 @@
+var Translation = {
+    "pt-BR": {
+        "header":{
+            "home-link": "home",
+            "menu": {
+                "projetos-link": "projetos",
+                "contato-link": "contato" 
+            }
+        },
+        "home-hero":{
+            "titulo": "AppSec Analyst",
+            "nome": "Gustavo Schmidt",
+            "desc": "Desenvolvedor de software focado em segurança, tratando de assuntos como, codigo seguro, mapeamento de dados e adequação legal de sistamas, estudando atualmente na PUCPR.",
+            "contato-bt": "contato"
+        },
+        "sobre":{
+            "titulo": "Sobre",
+            "texto": [
+                "Desde de pequeno gosto de tecnologia e fuçar as coisas, meu primeiro contato com computadores foi por volta dos meus 8 anos de idade, e o primeiro com programação com 12 anos, este com java, sem saber absolutamente nada tentando rodar servidores de minecraft.",
+                "Passei por diferentes experiencias com computação durante o ensino medio, participando das equipes de robotica, iniciação cientifica, e outros projetos desenvolvidos durante esse periodo, nesse periodo me aproximei mais da área se segurança, de desde então venho desenvolvendo projetos propios e me mergulhando em cibersegurança, sem abandonar minha paixão por desenvolvimento de software."   
+            ]
+        },
+        "projetos":{
+            "titulo": "projetos",
+            "vermais-bt": "veja outros projetos"
+        },
+        "stack":{
+            "titulo": "Tecnologias favoritas"
+        },
+        "footer":{
+            "update-msg": "Atualizado em janeiro de 2022"
+        }
+    },
+
+
+    "en":{
+        "header":{
+            "home-link": "home",
+            "menu": {
+                "projetos-link": "Projects",
+                "contato-link": "Contact" 
+            }
+        },
+        "home-hero":{
+            "titulo": "AppSec Analyst",
+            "nome": "Gustavo Schmidt",
+            "desc": "Software developer focused on security, dealing with issues such as secure code, data mapping and compliance of systems, i currently studying at PUCPR.",
+            "contato-bt": "Contact"
+        },
+        "sobre":{
+            "titulo": "About-me",
+            "texto": [
+                "Since I was little fond of technology and tinkering with things, my first contact with computers was around 8 years old, and the first with programming at 12 years old, this one with java, not knowing absolutely anything trying to run minecraft servers.",
+                "I went through different experiences with computing during high school, participating in robotics teams, scientific initiation, and other projects developed during this period, in this period I got closer to the security area, since then I have been developing my own projects and diving into cybersecurity. , without abandoning my passion for software development."   
+            ]
+        },
+        "projetos":{
+            "titulo": "Projects",
+            "vermais-bt": "View more"
+        },
+        "stack":{
+            "titulo": "favorite technologies"
+        },
+        "footer":{
+            "update-msg": "Updated January 2022"
+        }
+    }
+};
