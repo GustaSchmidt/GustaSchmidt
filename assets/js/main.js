b74c9cfed96bbb9f906413fcfe39fada4f9b51dd
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2022-03-01 20:05:06 -0300

    Add loader in projects section

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,48 @@ var configs = {
     ]
 
 };
+function loadProjects(){
+    var modelHTML = `
+        <li>
+            <img src="./assets/captura_de_tela.png" alt="">
+            <div class="conteiner-desc">
+                <h3>Titulo do projeto</h3>
+                <div class="project-tags">  
+                    <span>Lang</span>
+                </div>
+                <p class="desc">
+                    Lorem Ipsum
+                </p>
+            </div>
+        </li>
+    `;
+    for (let index = 0; index < configs.projetos.length; index++) {
+        const project = configs.projetos[index];
+        
+        //Verifica se foi passado usuario na url do git
+        var projectURL = project.github.split(/\//);
+        if(projectURL.length == 2){
+            projectURL = project.github;
+        }else{
+            projectURL = configs.github_user+'/'+project.github;
+        }
 
+        //Request API github
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var response = JSON.parse(xhttp.responseText);
+                console.log(response);
+                //Monta repositorio no layout
+
+            }
+        };
+        xhttp.open("GET", "https://api.github.com/repos/"+projectURL, true);
+        xhttp.send();
+        
+    }
+
+}
 document.addEventListener("DOMContentLoaded", function(event) {
     // Definindo links do footer
     var emailLink = document.querySelector("#mail-link");
