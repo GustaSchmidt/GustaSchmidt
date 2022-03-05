63ceaa6d8c17e480eeb96f3d1dae147c671c8af3
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2022-03-05 20:54:37 -0300

    [hotfix] ilustra colors

--- assets/js/colorMode.js
+++ assets/js/colorMode.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", function(event){
     const temaEscuro = window.matchMedia('(prefers-color-scheme: light)');
     var htmlRoot = document.querySelector('html');
 
+    var ilustra = document.getElementsByClassName("ilustra")[0];
+
     if(temaEscuro.matches){
         htmlRoot.classList.add('light-mode');
+        ilustra.src = "./assets/img/ilustra_purple.svg";
     }else{
         htmlRoot.classList.add('dark-mode');
-        
+        ilustra.src = "./assets/img/ilustra_blue.svg";
     }
 });
