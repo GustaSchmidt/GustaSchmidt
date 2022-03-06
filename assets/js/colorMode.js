87cee2a7defcda4abf94a23c42062ac8168aa2d9
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2022-03-06 14:51:17 -0300

    [New Feature] Theme switcher

--- assets/js/colorMode.js
+++ assets/js/colorMode.js
@@ -1,14 +1,45 @@
 document.addEventListener("DOMContentLoaded", function(event){
     const temaEscuro = window.matchMedia('(prefers-color-scheme: light)');
-    var htmlRoot = document.querySelector('html');
-
-    var ilustra = document.getElementsByClassName("ilustra")[0];
 
     if(temaEscuro.matches){
-        htmlRoot.classList.add('light-mode');
-        ilustra.src = "./assets/img/ilustra_purple.svg";
+        tema("light");
+        
     }else{
-        htmlRoot.classList.add('dark-mode');
-        ilustra.src = "./assets/img/ilustra_blue.svg";
+        tema("dark");
     }
+
+    //instancia do switch
+    var switcherTheme = document.getElementById('toggle_checkbox');
+    switcherTheme.addEventListener("change", function(e){   
+        var htmlRoot = document.querySelector('html');
+        if(htmlRoot.classList.contains("dark-mode")){
+            tema("light");
+        }else if(htmlRoot.classList.contains("light-mode")){
+            tema("dark");
+        }
+    });
 });
+function tema(theme){
+    var htmlRoot = document.querySelector('html');
+    var ilustra = document.getElementsByClassName("ilustra")[0];
+    console.log(htmlRoot);
+    htmlRoot.classList.remove("dark-mode");
+    htmlRoot.classList.remove("light-mode");
+
+    switch (theme) {
+        case "dark":
+            htmlRoot.classList.add('dark-mode');
+            ilustra.src = "./assets/img/ilustra_blue.svg";
+            break;
+
+        case "light":
+            htmlRoot.classList.add('light-mode');
+            ilustra.src = "./assets/img/ilustra_purple.svg";
+            break;
+    
+        default:
+            console.warn("thema não existe tema padrão dark");
+            tema("dark");
+            break;
+    }
+}
